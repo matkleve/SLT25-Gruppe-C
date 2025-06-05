b4cf3b613f2d5599b673d63fb736347d56f5ed5a
wi24b082 <wi24b082@technikum-wien.at>
2025-06-05 15:14:05 +0200

    Player added

--- TicTacSLT/src/main/java/org/example/Player.java
+++ TicTacSLT/src/main/java/org/example/Player.java
@@ -2,4 +2,15 @@ package org.example;
 
 public class Player {
 
+    private char symbol;
+
+    public Player( char symbol) {
+        this.symbol = symbol;
+    }
+
+
+
+    public char getSymbol() {
+        return symbol;
+    }
 }
