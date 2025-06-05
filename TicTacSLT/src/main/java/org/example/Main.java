fbc2be8bbc822a84bb0d3bfb77cd32c80f5af791
wi24b082 <wi24b082@technikum-wien.at>
2025-06-05 15:20:04 +0200

    Players added in Main

--- TicTacSLT/src/main/java/org/example/Main.java
+++ TicTacSLT/src/main/java/org/example/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         board.printBoard(); // Basic rendering
+
+        Player player1 = new Player( 'X');
+        Player player2 = new Player('O');
+
+
+
+
     }
 }
